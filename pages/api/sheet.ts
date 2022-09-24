59f17f5ac2d8b2dcc36e07e3f27c6da71b1f8489
Deepscope <dev@deepscope.co.uk>
2022-09-24 19:48:54 +0100

    First commit

--- pages/api/sheet.ts
+++ pages/api/sheet.ts
@@ -9,5 +9,11 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+    if(req.method==='GET'){
+    res.status(200).json({ name: 'Testing' })
+    
+    } else if(req.method==='POST'){
+      console.log(console.log(req.body.row));
+      res.status(200);  
+    }
 }
